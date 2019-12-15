3cdebf2544ef0023d29fb345a84f0959774956d9
BlackChang <dnjs7651@gmail.com>
2019-12-16 02:33:49 +0900

    Create battery_main_widget.dart

---
+++ lib/widget/battery_main_widget.dart
@@ -0,0 +1,92 @@
+import 'package:battery/data/battery_state_data.dart';
+import 'package:battery/Model/battery_state.dart';
+import 'package:flutter/material.dart';
+
+class RecoveryWidget extends StatefulWidget {
+  @override
+  RecoveryWidgetState createState() => RecoveryWidgetState();
+}
+
+class RecoveryWidgetState extends State<RecoveryWidget> {
+  bool ascending;
+  List<Battery> selectedBatteries;
+
+  @override
+  void initState() {
+    super.initState();
+
+    ascending = false;
+    selectedBatteries = [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: SizedBox(
+        width: width * 1,
+        child: ListView(
+          children: <Widget>[
+            buildDataTable(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildDataTable() => DataTable(
+        sortAscending: ascending,
+        sortColumnIndex: 1,
+        columns: batteryColumns.sublist(0, 3)
+            .map(
+              (String column) => DataColumn(
+                    label: Text(column),
+                    onSort: (int columnIndex, bool ascending) => onSortColumn(
+                        columnIndex: columnIndex, ascending: ascending),
+                  ),
+            )
+            .toList(),
+        rows: batteries
+            .map((Battery battery) => DataRow(
+                  selected: selectedBatteries.contains(battery),
+                  cells: [
+                    DataCell(Text('${battery.batteryID}')),
+                    DataCell(Text('${battery.state}')),
+                    DataCell(Text('${battery.dueDate}')),
+                  ],
+                  // onSelectChanged: (bool selected) =>
+                  //     onSelectedRowChanged(selected: selected, battery: battery),
+                ))
+            .toList(),
+      );
+
+  void onSortColumn({int columnIndex, bool ascending}) {
+    if (columnIndex == 1) {
+      setState(() {
+        if (ascending) {
+          batteries.sort((a, b) => a.state.compareTo(b.state));
+        } else {
+          batteries.sort((a, b) => b.state.compareTo(a.state));
+        }
+        this.ascending = ascending;
+      });
+    }
+  }
+
+  void onSelectedRowChanged({bool selected, Battery battery}) {
+    setState(() {
+      if (selected) {
+        selectedBatteries.add(battery);
+      } else {
+        selectedBatteries.remove(battery);
+      }
+    });
+  }
+
+  Function onPressed() {
+    if (selectedBatteries.isEmpty) return null;
+
+    return () {};
+  }
+}
